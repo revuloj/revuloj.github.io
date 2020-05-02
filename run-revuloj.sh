b77c99142437ece95e7029f2ebabd7cc894fc2df
Wolfram Diestel <diestel@steloj.de>
2020-05-02 18:14:34 +0200

    alinomu src->docs

--- run-revuloj.sh
+++ run-revuloj.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -it -p 4000:4000 -v $(pwd)/src:/usr/src/app/revuloj.github.io revuloj.github.io \
+docker run --rm -it -p 4000:4000 -v $(pwd)/docs:/usr/src/app/revuloj.github.io revuloj.github.io \
   bundle exec jekyll serve --watch --force_polling --host 0.0.0.0 $@
