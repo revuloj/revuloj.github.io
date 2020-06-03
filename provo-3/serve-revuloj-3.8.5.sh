2354be88658f63cd7dd9e590c712a550292485b2
Wolfram Diestel <wolfram.diestel@t-systems.com>
2020-06-03 16:22:11 +0200

    alia provo fari procezujon surbaze de jekyll:3.8.5

---
+++ provo-3/serve-revuloj-3.8.5.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# https://github.com/envygeeks/jekyll-docker/blob/master/README.md
+
+#docker run --rm \
+#  --volume="$PWD:/srv/jekyll" \
+#  -it jekyll/jekyll:$JEKYLL_VERSION \
+#  jekyll build
+
+
+#export JEKYLL_VERSION=3.8
+#docker run --rm \
+#  --volume="$PWD:/srv/jekyll" \
+#  -it jekyll/jekyll:$JEKYLL_VERSION \
+#  bundle update
+
+docker run --rm -it -p 4000:4000 -v $(pwd):/srv/jekyll revuloj.github.io jekyll build
+
+#  bundle exec jekyll serve --watch --force_polling --host 0.0.0.0 $@
