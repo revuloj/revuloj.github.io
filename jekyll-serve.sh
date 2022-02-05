39c7ffb5218d5f8c7c3098fa9e3a2ed5f1ac3eb5
Wolfram Diestel <diestel@steloj.de>
2022-02-05 15:10:24 +0100

    -kor. malbonajn prezentojn de spcialsignoj en manlibro

--- jekyll-serve.sh
+++ jekyll-serve.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-jekyll serve --trace
+echo "Tiu skripto servas la retpaĝoj loke uzante jekyll - simile kiel tio okazas en la publika paĝo"
+echo "Ĉiu konservita pago tiam tuj konvertiĝas kaj estas spektebla en retumilo ĉe http://0.0.0.0:4000"
+echo ""
+echo "Unue necesas instali pakaĵoj laŭ la enhavo en Gemfile kaj antaŭe certigu aktualigitan"
+echo "sistemon:"
+echo "# sudo gem update --system"
+echo "# bundle install"
 
+# Jen la kompleta komando por uzo de 'bundle', vi povas aldoni argumenton --trace se vi vokas la skripton
+bundle exec jekyll serve --watch --force_polling --host 0.0.0.0 $@
 
+# Se vi alie instalas ĉiujn bezonatajn pakaĵojn eventuale funkcias ankaŭ simpla:
+# jekyll serve --trace
